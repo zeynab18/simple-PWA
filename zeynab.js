dbc8b078f79f62c730ad52403edeea9444d65176
zey <zeynabmasoumi18@gmail.com>
2024-11-19 15:50:07 +0330

    tamrinaval

---
+++ zeynab.js
@@ -0,0 +1,13 @@
+/*
+const x=12
+const y=15
+const z=20
+console.log((x+y+z)/3)
+let name='zey'
+let family='masoumi'
+console.log(name+' '+family)
+const arr=[1,8,"reza",18]
+console.log(arr[2])
+*/
+const ob = { "name": "zeynab", "age": 18, description: "test content" }
+console.log(ob.age)
